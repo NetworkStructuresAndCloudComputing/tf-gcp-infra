e1e73ebf15eff48b08a669bff60d488c850ca7fe
MohammedZakirMemon <memon.m@northeastern.edu>
2024-02-12 22:04:58 -0500

    Adding Terraform implementation

---
+++ varaibles.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type        = string
+  description = "Google Cloud region"
+}
+
+variable "project_id" {
+  type        = string
+  description = "Google Cloud project ID"
+}
