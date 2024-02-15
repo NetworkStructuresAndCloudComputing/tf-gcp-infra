928344c0a115192074f34b5433cfdf3f639d6a4d
MohammedZakirMemon <memon.m@northeastern.edu>
2024-02-15 01:51:51 -0500

    updating route

--- routes.tf
+++ routes.tf
@@ -1,5 +1,5 @@
-resource "google_compute_route" "webapp_route" {
-  name               = "webapp-route"
+resource "google_compute_route" "default_route" {
+  name               = "default-route"
   network            = google_compute_network.cloudcomputing_vpc.self_link
   dest_range         = "0.0.0.0/0"
   next_hop_gateway    = "default-internet-gateway"
