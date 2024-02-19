2e811540633e5998b6c46f88aff8566a4086150f
MohammedZakirMemon <memon.m@northeastern.edu>
2024-02-18 22:20:36 -0500

    adding tfvars variables

--- routes.tf
+++ routes.tf
@@ -1,6 +1,6 @@
 resource "google_compute_route" "default_route" {
-  name               = "default-route"
+  name               = var.default_route_name
   network            = google_compute_network.cloudcomputing_vpc.self_link
-  dest_range         = "0.0.0.0/0"
-  next_hop_gateway    = "default-internet-gateway"
+  dest_range         = var.route_dest_range
+  next_hop_gateway    = var.route_next_hop_gateway
 }
