61882e6d1fdbb1e684e19c484c900ec2841fb7a3
MohammedZakirMemon <memon.m@northeastern.edu>
2024-02-26 02:09:33 -0500

    adding new project id

--- routes.tf
+++ routes.tf
@@ -1,6 +1,6 @@
 resource "google_compute_route" "default_route" {
-  name               = var.default_route_name
-  network            = google_compute_network.cloudcomputing_vpc.self_link
-  dest_range         = var.route_dest_range
-  next_hop_gateway    = var.route_next_hop_gateway
+  name             = var.default_route_name
+  network          = google_compute_network.cloudcomputing_vpc.self_link
+  dest_range       = var.route_dest_range
+  next_hop_gateway = var.route_next_hop_gateway
 }
